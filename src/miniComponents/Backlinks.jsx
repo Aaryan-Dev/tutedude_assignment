246643eec867ec70e6a3e090dcc1392dec355c71
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-04 00:23:38 +0530

    Update Backlinks.jsx

--- src/miniComponents/Backlinks.jsx
+++ src/miniComponents/Backlinks.jsx
@@ -5,7 +5,9 @@ const Backlinks = () => {
   return (
     <>
       <div className={css.linktags} id={css.Backlinks}>
-        <a href="google.com">UI/UX</a>
+        <a href="google.com">
+          <pre> UI/UX</pre>
+        </a>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -19,7 +21,9 @@ const Backlinks = () => {
             d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
           />
         </svg>
-        <a href="google.com">Refer & Earn</a>
+        <a href="google.com">
+          <pre>Refer & Earn</pre>
+        </a>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -33,11 +37,30 @@ const Backlinks = () => {
             d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
           />
         </svg>
-        <a href="google.com">Friends Referred</a>
+        <a href="google.com">
+          <pre>Friends Referred</pre>
+        </a>
       </div>
 
       <div id={css.back}>
-        <div>go back</div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            />
+          </svg>
+          <div>
+            <pre> go back</pre>
+          </div>
+        </div>
       </div>
     </>
   );
