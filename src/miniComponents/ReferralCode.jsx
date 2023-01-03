844b395bbce268953a3d0453874ccb0a330bd4a7
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-03 19:11:28 +0530

    Update ReferralCode.jsx

--- src/miniComponents/ReferralCode.jsx
+++ src/miniComponents/ReferralCode.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import css from "../my.module.css";
 
 const ReferralCode = () => {
-  return <div>ReferralCode</div>;
+  return (
+    <div id={css.ReferralCode}>
+      <div className={css.enrolled}>
+        <pre>Friends who enrolled</pre>
+        <p>(3)</p>
+      </div>
+
+      <div className={css.friends}>
+        <div>
+          <div>
+            <div className={css.name}>Dhiraj Saxsena</div>
+            <div className={css.date}>14 Sep, 2022</div>
+          </div>
+          <div className={css.detail}>Courses Enrolled(6)</div>
+          <div className={css.skills}>
+            <div>UI/UX</div>
+            <div>Photoshop</div>
+            <div>Illustrator</div>
+            <div>Python</div>
+            <div>MERN</div>
+          </div>
+          <div>
+            <pre className={css.amount}>Referral Amount</pre>
+            <p className={css.price}>&#8377; 185</p>
+          </div>
+        </div>
+      </div>
+
+      <div id={css.terms}>Terms & Conditions</div>
+    </div>
+  );
 };
 
 export default ReferralCode;
