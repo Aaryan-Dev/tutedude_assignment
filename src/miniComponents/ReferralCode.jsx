f12a699034ff13519fd1d19a876117e805b5b71b
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-03 20:52:09 +0530

    Update ReferralCode.jsx

--- src/miniComponents/ReferralCode.jsx
+++ src/miniComponents/ReferralCode.jsx
@@ -28,6 +28,42 @@ const ReferralCode = () => {
             <p className={css.price}>&#8377; 185</p>
           </div>
         </div>
+        <div>
+          <div>
+            <div className={css.name}>Dhiraj Saxsena</div>
+            <div className={css.date}>14 Sep, 2022</div>
+          </div>
+          <div className={css.detail}>Courses Enrolled(6)</div>
+          <div className={css.skills}>
+            <div>UI/UX</div>
+            <div>Photoshop</div>
+            <div>Illustrator</div>
+            <div>Python</div>
+            <div>MERN</div>
+          </div>
+          <div>
+            <pre className={css.amount}>Referral Amount</pre>
+            <p className={css.price}>&#8377; 185</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className={css.name}>Dhiraj Saxsena</div>
+            <div className={css.date}>14 Sep, 2022</div>
+          </div>
+          <div className={css.detail}>Courses Enrolled(6)</div>
+          <div className={css.skills}>
+            <div>UI/UX</div>
+            <div>Photoshop</div>
+            <div>Illustrator</div>
+            <div>Python</div>
+            <div>MERN</div>
+          </div>
+          <div>
+            <pre className={css.amount}>Referral Amount</pre>
+            <p className={css.price}>&#8377; 185</p>
+          </div>
+        </div>
       </div>
 
       <div id={css.terms}>Terms & Conditions</div>
