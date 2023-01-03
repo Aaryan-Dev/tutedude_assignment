601227db67431c452c31ed27541c933016196100
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-03 17:43:21 +0530

    Update EnrolledFriends.jsx

--- src/miniComponents/EnrolledFriends.jsx
+++ src/miniComponents/EnrolledFriends.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import css from "../my.module.css";
 
-const enrolledFriends = () => {
-  return <div>enrolledFriends</div>;
+const EnrolledFriends = () => {
+  return (
+    <div className={css.enroll}>
+      <div className={css.code}>
+        <div className={css.referral}>Your Referral Code</div>
+
+        <div className={css.coder}>
+          <pre>EDCH54</pre>
+        </div>
+      </div>
+
+      <div className={css.wall}>
+        <div className={css.wallet}>Wallet Balance</div>
+        <div className={css.ruppee}>&#8377; 500 </div>
+      </div>
+    </div>
+  );
 };
 
-export default enrolledFriends;
+export default EnrolledFriends;
