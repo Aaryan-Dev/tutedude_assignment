e254c7526982f1f31becd0cbe39f9104041f3792
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-04 00:23:37 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,17 +20,36 @@ const Navbar = () => {
           <div className={css.profileName}>
             <div>
               <img
+                id={css.prof}
                 src="https://i.postimg.cc/m2ZSKv2T/Vector.png"
                 alt="vector"
               />
             </div>
-            <div>
+            <div className={css.insert}>
               <a className={css.profile} href="google.com">
                 <pre> ProfileName </pre>
+
+                <div id={css.hide_arrow}>
+                  <svg
+                    id={css.d_down}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-chevron-down"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                    />
+                  </svg>
+                </div>
               </a>
             </div>
             <div>
               <img
+                className={css.drop_down}
                 src="https://i.postimg.cc/DfPgjvhC/Vector-drop.png"
                 alt="dropdown"
               />
