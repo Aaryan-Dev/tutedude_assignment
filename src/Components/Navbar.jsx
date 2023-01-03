e2685ba24b0cfc6f5ba2b84cebeec7f952d48abd
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-03 16:10:06 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,24 +3,39 @@ import css from "../my.module.css";
 
 const Navbar = () => {
   return (
-    <div id={css.navbar}>
-      <div>
-        <img src="https://i.postimg.cc/cLv1kLpw/image-1.png" alt="logo" />
-      </div>
-      <div className={css.links}>
-        <div>
-          <a href="google.com">My Assignment</a>
-        </div>
-        <div>
-          <a href="google.com">Chat with Mentor</a>
-        </div>
-        <div>
-          <img src="https://i.postimg.cc/m2ZSKv2T/Vector.png" alt="vector" />
-          <a href="google.com">ProfileName</a>
-          <img
-            src="https://i.postimg.cc/DfPgjvhC/Vector-drop.png"
-            alt="dropdown"
-          />
+    <div className={css.navigator}>
+      <div id={css.navbar}>
+        <div id={css.logo}></div>
+        <div className={css.links}>
+          <div>
+            <a className={css.assignment} href="google.com">
+              <pre>My Assignment</pre>
+            </a>
+          </div>
+          <div>
+            <a className={css.assignment} href="google.com">
+              <pre>Chat with Mentor</pre>
+            </a>
+          </div>
+          <div className={css.profileName}>
+            <div>
+              <img
+                src="https://i.postimg.cc/m2ZSKv2T/Vector.png"
+                alt="vector"
+              />
+            </div>
+            <div>
+              <a className={css.profile} href="google.com">
+                <pre> ProfileName </pre>
+              </a>
+            </div>
+            <div>
+              <img
+                src="https://i.postimg.cc/DfPgjvhC/Vector-drop.png"
+                alt="dropdown"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
