d708ec26d4eeaa7a6bc79c744b20529b8143bc7a
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2023-01-03 16:35:23 +0530

    Update Backlinks.jsx

--- src/miniComponents/Backlinks.jsx
+++ src/miniComponents/Backlinks.jsx
@@ -2,7 +2,41 @@ import React from "react";
 import css from "../my.module.css";
 
 const Backlinks = () => {
-  return <div>Backlinks</div>;
+  return (
+    <div id={css.Backlinks}>
+      {/* <div> */}
+      <a href="google.com">UI/UX</a>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-chevron-right"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+      <a href="google.com">Refer & Earn</a>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-chevron-right"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+      <a href="google.com">Friends Referred</a>
+      {/* </div> */}
+    </div>
+  );
 };
 
 export default Backlinks;
